7e5ff1fc1a50371846fc1d11c2a43cfe0b570521
AjayNSS <kaleajay@yahoo.comgit config --global user.email kaleajay@yahoo.comgit config --global user.email kaleajay@yahoo.com>
2023-02-09 21:34:01 -0600

    thru ques 4

--- lahman_baseball.sql
+++ lahman_baseball.sql
@@ -7,42 +7,59 @@
 -- salary they earned in the major leagues. Sort this list in descending order by 
 -- the total salary earned. Which Vanderbilt player earned the most money in the majors?
 
-	select distinct
+	SELECT DISTINCT
 		playerid
 		,namefirst
 		,namelast
-	,SUM(sal.salary) as tot_salary
+		,SUM(sal.salary) as tot_salary
 		--,max(sal.yearid) as max_year
-	from people p
-	INNER JOIN collegeplaying cp using (playerid)
-	inner join schools s using (schoolid )
-	inner join salaries sal using (playerid )
-	where schoolname like 'Vanderbilt University'
-	group by playerid
+	FROM people p
+		INNER JOIN collegeplaying cp USING (playerid)
+		INNER JOIN schools s USING (schoolid )
+		INNER JOIN salaries sal USING (playerid )
+	WHERE schoolname LIKE 'Vanderbilt University'
+	GROUP BY playerid
 		,namefirst
 		,namelast
-	order by tot_salary desc
+	ORDER BY tot_salary DESC
 
-
+--Correct
+WITH vandy_players AS (
+						SELECT DISTINCT playerid
+						FROM collegeplaying 
+							LEFT JOIN schools
+							USING(schoolid)
+						WHERE schoolid = 'vandy'
+)
+SELECT namefirst, 
+	   namelast, 
+	   SUM(salary)::numeric::money AS total_salary, 
+	   COUNT(DISTINCT yearid) AS years_played
+FROM people
+	 INNER JOIN vandy_players
+	 USING(playerid)
+	 INNER JOIN salaries
+	 USING(playerid)
+GROUP BY playerid, namefirst, namelast
+ORDER BY total_salary DESC;
 
 -- 2. Using the fielding table, group players into three groups based on their position: 
 -- label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", 
 -- and those with position "P" or "C" as "Battery". 
 -- Determine the number of putouts made by each of these three groups in 2016.
-SELECT 
-namefirst
-,namelast
-,CASE WHEN f.pos = 'OF' THEN 'Outfield'
-	WHEN  f.pos = 'SS' OR  f.pos = '1B' OR  f.pos = '2B' OR  f.pos = '3B' THEN 'Infield'
-	WHEN  f.pos = 'P' OR  f.pos= 'C' THEN 'Battery'
-END AS pos
-, SUM(PO)
-FROM people p 
-INNER JOIN fielding f using(playerid)
-WHERE yearid =2016
-GROUP BY namefirst, namelast, pos
 
 
+SELECT SUM(po), 
+		CASE WHEN f.pos = 'OF' THEN 'Outfield'
+			WHEN  f.pos = 'SS' OR  f.pos = '1B' OR  f.pos = '2B' OR  f.pos = '3B' THEN 'Infield'
+			WHEN  f.pos = 'P' OR  f.pos= 'C' THEN 'Battery'
+		END AS g_pos
+FROM fielding f
+WHERE yearid = '2016'
+GROUP BY g_pos;
+
+select distinct yearid from public.batting
+order by 1 
 
 -- 3. Find the average number of strikeouts per game by decade since 1920. 
 -- Round the numbers you report to 2 decimal places. Do the same for home runs per game. 
@@ -50,6 +67,30 @@ GROUP BY namefirst, namelast, pos
 -- at the **generate_series** function (https://www.postgresql.org/docs/9.1/functions-srf.html). 
 -- If you want to see an example of this in action, check out this DataCamp 
 -- video: https://campus.datacamp.com/courses/exploratory-data-analysis-in-sql/summarizing-and-aggregating-numeric-data?ex=6)
+
+WITH decade AS (SELECT 
+generate_series (1920, 2016, 10) AS decade_group)
+	SELECT decade_group,
+	COALESCE(ROUND (SUM(so)*1.0/SUM(g), 2), 0) as AvgSO_game,
+	COALESCE(ROUND (SUM(hr)*1.0/SUM(g), 2), 0) as AvgHR_game
+	FROM pitching
+	INNER JOIN decade
+		ON decade_group+1 <= yearid 
+		AND decade_group+10 >= yearid
+		WHERE yearid >= 1920
+		GROUP BY decade_group
+	ORDER BY decade_group ASC;
+
+--Games
+	WITH generate_series AS (SELECT * FROM
+		  generate_series(1920,2010,10 ))
+	SELECT generate_series, SUM(g) 
+	FROM pitching
+	INNER JOIN generate_series
+	ON generate_series+1 <= yearid AND generate_series+10 >= yearid
+	GROUP BY generate_series
+	ORDER BY generate_series DESC
+
 
 -- 4. Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases. Report the players' names, number of stolen bases, number of attempts, and stolen base percentage.
 
